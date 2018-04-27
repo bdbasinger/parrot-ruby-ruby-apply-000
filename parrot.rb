ecace2509fd4b105618c1d19aa56d120614d18d0
Brennan B <bdbasinger777@gmail.com>
2018-04-27 03:18:50 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(str)
+  if str.nil?
+    puts "Squawk!"
+    return "Squawk!"
+  else
+    puts str
+    return str
+  end
+end
+
+  
+  
