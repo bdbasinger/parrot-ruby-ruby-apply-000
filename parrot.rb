bd2236cfbc4346855107ecba8b59fb56423d1680
Brennan B <bdbasinger777@gmail.com>
2018-04-27 03:26:25 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,14 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(str)
-  if str != "Squawk!" 
-    puts str
-    return str
-  else
+def parrot(str="Squawk!"))
+  if str == "Squawk!"
     puts "Squawk!"
     return "Squawk!"
-  end
 end
 
   
